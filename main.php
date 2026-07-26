ebdcb274b32297b56c231906f3f463ac697c8bc8
B1eL <engs-gabrielcaldas@camporeal.edu.br>
2026-07-25 22:41:45 -0300

    Atividade 3

--- main.php
+++ main.php
@@ -1,6 +1,8 @@
 <?php
 
 // Atividade 1
+echo "--- Atividade 1 ---" . "\n";
+
 $idade = 0;
 if ($idade >= 18) {
     echo "Você eh maior de idade!" . "\n";
@@ -9,6 +11,8 @@ if ($idade >= 18) {
 }
 
 // Atividade 2
+echo "--- Atividade 2 ---" . "\n";
+
 $dinheiro = 5;
 
 echo "Você tem R$" . $dinheiro . ", é um...\n";
@@ -30,4 +34,32 @@ switch ($dinheiro) {
         break;
     default:
         echo "Nem dinheiro você tem!" . "\n";
+}
+
+// Atividade 3
+echo "--- Atividade 3 ---" . "\n";
+
+$numero1  = 20;
+$numero2  = 10;
+$operacao = "soma";
+
+switch ($operacao) {
+    case "soma":
+        echo "A soma de $numero1 e $numero2 é: " . ($numero1 + $numero2) . "\n";
+        break;
+    case "subtracao":
+        echo "A subtração de $numero1 e $numero2 é: " . ($numero1 - $numero2) . "\n";
+        break;
+    case "multiplicacao":
+        echo "A multiplicação de $numero1 e $numero2 é: " . ($numero1 * $numero2) . "\n";
+        break;
+    case "divisao":
+        if ($numero2 != 0) {
+            echo "A divisão de $numero1 e $numero2 é: " . ($numero1 / $numero2) . "\n";
+        } else {
+            echo "Não é possível dividir por zero!" . "\n";
+        }
+        break;
+    default:
+        echo "Operação inválida!" . "\n";
 }
